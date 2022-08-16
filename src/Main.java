2d3965a49fc389e8c8980032c2b2f6de011666ad
Hazal Nur Akyuz <hazalnurakyuz@outlook.com>
2022-08-16 14:48:14 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double boy,kilo,indeks;
+
+        Scanner scr = new Scanner(System.in);
+
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
+        boy = scr.nextDouble();
+
+        System.out.print("Lütfen kilonuzu giriniz : ");
+        kilo = scr.nextDouble();
+
+        indeks = kilo / (boy * boy);
+        System.out.println("Vücut Kitle İndeksiniz :" + indeks);
+
     }
-}
+    }
